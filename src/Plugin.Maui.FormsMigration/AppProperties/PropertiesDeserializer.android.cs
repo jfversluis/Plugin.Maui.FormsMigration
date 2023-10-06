fefc58440686c82d761cb1bbfa75222a080df166
Gerald Versluis <gerald.versluis@microsoft.com>
2023-10-06 12:00:24 +0200

    Add AppProperties Initial version

---
+++ src/Plugin.Maui.FormsMigration/AppProperties/PropertiesDeserializer.android.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace Plugin.Maui.FormsMigration.Properties;
+
+public static class PropertiesDeserializer
+{
+    const string propertyStoreFile = "PropertyStore.forms";
+
+    public static Task<IDictionary<string, object>> DeserializePropertiesAsync()
+    {
+        // Deserialize property dictionary to local storage
+        return Task.Run(() =>
+        {
+			using IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication();
+			if (!store.FileExists(propertyStoreFile))
+			{
+				return new Dictionary<string, object>(4);
+			}
+
+			using IsolatedStorageFileStream stream = store.OpenFile(propertyStoreFile, FileMode.Open, FileAccess.Read);
+			using XmlDictionaryReader reader = XmlDictionaryReader.CreateBinaryReader(stream, XmlDictionaryReaderQuotas.Max);
+			
+            if (stream.Length == 0)
+            {
+				return new Dictionary<string, object>(4);
+            }
+
+			try
+			{
+				var dcs = new DataContractSerializer(typeof(Dictionary<string, object>));
+				var readObject = dcs.ReadObject(reader) as IDictionary<string, object>;
+				return readObject ?? new Dictionary<string, object>(4);
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine("Could not deserialize properties: " + e.Message);
+				Console.WriteLine($"PropertyStore Exception while reading Application properties: {e}");
+			}
+			
+			return new Dictionary<string, object>(4);
+        });
+    }
+}
---
+++ src/Plugin.Maui.FormsMigration/AppProperties/PropertiesDeserializer.ios.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using System.IO.IsolatedStorage;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace Plugin.Maui.FormsMigration.Properties;
+
+public static class PropertiesDeserializer
+{
+    const string propertyStoreFile = "PropertyStore.forms";
+
+    public static Task<IDictionary<string, object>> DeserializePropertiesAsync()
+    {
+        // Deserialize property dictionary to local storage
+        return Task.Run(() =>
+        {
+			using var store = IsolatedStorageFile.GetUserStoreForApplication();
+			using var stream = store.OpenFile(propertyStoreFile, FileMode.OpenOrCreate);
+			using var reader = XmlDictionaryReader.CreateBinaryReader(stream, XmlDictionaryReaderQuotas.Max);
+
+			if (stream.Length == 0)
+            {
+				return new Dictionary<string, object>(4);
+            }
+
+			try
+			{
+				var dcs = new DataContractSerializer(typeof(Dictionary<string, object>));
+				var readObject = dcs.ReadObject(reader) as IDictionary<string, object>;
+				
+				return readObject ?? new Dictionary<string, object>(4);
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine("Could not deserialize properties: " + e.Message);
+				Console.WriteLine($"PropertyStore Exception while reading Application properties: {e}");
+			}
+
+			return new Dictionary<string, object>(4);
+        });
+    }
+}
